9be3d5cc3684ce2d239fac7259467a725305ba2e
Iago Barbosa <gliago_iago@hotmail.com>
2021-04-05 11:25:02 -0300

    2021-04-05 publicando o inicio do projeto

---
+++ alura-typer/public/js/placar.js
@@ -0,0 +1,54 @@
+$("#botao-placar").click(mostraPlacar);
+
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Douglas"
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.append(linha);
+    $(".placar").slideDown(500);
+    scrollPlacar();
+}
+
+function scrollPlacar() {
+    let positionPlacar = $(".placar").offset().top;
+    $("body").animate({
+        scrollTop: positionPlacar+"px"
+    }, 1000);
+}
+
+function novaLinha(usuario, palavras) {
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha() {
+    event.preventDefault();
+    let linha = $(this).parent().parent();
+    linha.fadeOut(800);
+    setTimeout(function() {
+        linha.remove();
+    }, 800);
+}
+
+function mostraPlacar () {
+    $(".placar").stop().slideToggle(800);
+}
